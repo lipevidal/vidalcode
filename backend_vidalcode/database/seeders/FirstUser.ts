da4224be0af317a5160ca7b202ae527548497e95
Filipe Vidal <vidaltecst@gmail.com>
2023-11-12 23:08:34 -0300

    Login/Logout

---
+++ backend_vidalcode/database/seeders/FirstUser.ts
@@ -0,0 +1,12 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import User from 'App/Models/User'
+
+export default class extends BaseSeeder {
+  public async run () {
+    // Write your database queries inside the run method
+    await User.create({
+      email: 'teste@gmail.com',
+      password: 'teste'
+    })
+  }
+}
